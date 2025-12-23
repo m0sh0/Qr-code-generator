9dcaddfa3d4a93a150f5b8e041c96db50345cf39
Mihail Mihov <mishomihov41@gmail.com>
2025-12-23 21:01:08 +0200

    Add initial QR code generator project and tests
    Created the QrCodeGeneratorProject solution with DTOs for QR code metadata and result, utility exception messages, and a basic Program for QR code generation using QRCoder. Added a test project with NUnit setup. Included .NET 6.0 configuration and initial IDE settings.

---
+++ QrCodeGenerator/QrCodeGeneratorProject/DTO/QrCodeResult.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using QrCodeGeneratorProject.Utilites;
+
+namespace QrCodeGeneratorProject.DTO;
+
+public class QrCodeResult
+{
+    private const string DefaultFileName = "QrCode";
+    private string? _suggestedFileName;
+    
+    public QrCodeResult(byte[] data, FormatTypes format)
+    {
+        this.Data = data;
+        this.Format = format;
+        this._suggestedFileName = $"{DefaultFileName}.{format.ToString().ToLower()}";
+    }
+
+    public QrCodeResult(byte[] data, FormatTypes format, string suggestedFileName)
+        : this(data, format)
+    {
+        this.SuggestedFileName = suggestedFileName;  
+    }
+    
+    
+    public byte[] Data { get; private set; }
+    public FormatTypes Format { get; private set; }
+
+    public string? SuggestedFileName
+    {
+        get => this._suggestedFileName;
+        private set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(ExceptionMessages.FileNameNullOrEmpty);
+            }
+
+            if (!CheckValidFilename(value) || value.StartsWith('.'))
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidCharacterInFilename);
+            }
+            
+            
+            if (value.LastIndexOf('.') == -1)
+            {
+                this._suggestedFileName = $"{value}.{this.Format.ToString().ToLower()}";
+            }
+            else
+            {
+                string correctedFilename = value.Remove(value.LastIndexOf('.'));
+                this._suggestedFileName = $"{correctedFilename}.{this.Format.ToString().ToLower()}";
+            }
+        }
+    }
+
+    
+    private bool CheckValidFilename(string filename)
+    {
+        if (filename.Any(c => Path.GetInvalidFileNameChars().Contains(c)))
+        {
+            return false;
+        }
+        
+        return true;
+    }
+}
---
+++ QrCodeGenerator/QrCodeGeneratorProject/Utilites/ExceptionMessages.cs
@@ -0,0 +1,8 @@
+﻿namespace QrCodeGeneratorProject.Utilites;
+
+public class ExceptionMessages
+{
+    public const string TextIsNullOrEmpty = "Text is null or empty.";
+    public const string FileNameNullOrEmpty = "Filename is  null or empty.";
+    public const string InvalidCharacterInFilename = "Invalid character in filename.";
+}
