1f8a9afa9496a7a30b1c6e540da1fa7cfd93baf9
Mihail Mihov <mishomihov41@gmail.com>
2026-01-04 22:30:35 +0200

    Did some messing around. Nothing productive.

--- QrCodeGenerator/QrCodeGeneratorProject/Program.cs
+++ QrCodeGenerator/QrCodeGeneratorProject/Program.cs
@@ -1,26 +1,30 @@
 ﻿using QRCoder;
-using System;
-using System.Drawing;
-using System.IO;
-using QrCodeGeneratorProject.Core;
-using QrCodeGeneratorProject.Core.Interfaces;
-using QrCodeGeneratorProject.DTO;
-using QrCodeGeneratorProject.Factory;
-using QrCodeGeneratorProject.Factory.Interfaces;
-using QrCodeGeneratorProject.IO;
-using QrCodeGeneratorProject.IO.Interfaces;
 
 namespace QrCodeGeneratorProject;
 
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        IWriter writer = new FileWriter();
-        IQrCodeFactory factory = new QrCodeFactory();
+        // IWriter writer = new FileWriter();
+        // IQrCodeFactory factory = new QrCodeFactory();
+        //
+        // IEngine engine = new Engine(writer,factory);
+        // engine.Run();
         
-        IEngine engine = new Engine(writer,factory);
-        engine.Run();
+        
+        var wifiPayload =  new PayloadGenerator.WiFi
+            (
+                ssid: "MyWifiNetwork",
+                password: "MyWifiPassword", 
+                PayloadGenerator.WiFi.Authentication.nopass, 
+                isHiddenSSID: false,
+                escapeHexStrings: true
+            );
+        
+        QRCodeData qrCodeData = QRCodeGenerator.GenerateQrCode(wifiPayload, QRCodeGenerator.ECCLevel.Q);
+        string qrCodeAsSvg = new SvgQRCode(qrCodeData).GetGraphic(20);
+        File.WriteAllText("../../../Wifi.svg",qrCodeAsSvg);
     }
 }
